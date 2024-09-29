170296a0b2974e2fc978d4cc2c069062756b1a96
tqdung11a3 <164484603+tqdung11a3@users.noreply.github.com>
2024-09-29 10:17:22 +0700

    Caculate.java

--- Caculate.java
+++ Caculate.java
@@ -4,9 +4,17 @@ public class Caculate {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		double num1 = sc.nextDouble(), num2 = sc.nextDouble();
-		System.out.printf("%.2f\n", num1 + num2);
-		System.out.printf("%.2f\n", num1 - num2);
-		System.out.printf("%.2f\n", num1 + num2);
-		System.out.printf("%.2f\n", num1/ num2);
+		if (num2 == 0) {
+			System.out.printf("%.2f\n", num1 + num2);
+			System.out.printf("%.2f\n", num1 - num2);
+			System.out.printf("%.2f\n", num1 + num2);
+			System.out.println("Khong the thuc hien phep chia");
+		}
+		else {
+			System.out.printf("%.2f\n", num1 + num2);
+			System.out.printf("%.2f\n", num1 - num2);
+			System.out.printf("%.2f\n", num1 + num2);
+			System.out.printf("%.2f\n", num1/ num2);
+		}
 	}
 }
